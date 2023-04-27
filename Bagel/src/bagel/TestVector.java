2675aa0861a69c1349d0acd8266d6ff16a6ba327
Gagan-30 <109915066+Gagan-30@users.noreply.github.com>
2023-04-27 16:08:33 +0100

    Add files via upload

---
+++ Bagel/src/bagel/TestVector.java
@@ -0,0 +1,24 @@
+package bagel;
+
+public class TestVector 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Testig vector");
+        
+        Vector v = new Vector(2,3);
+        System.out.println(v); // < 2 , 3 >
+        
+        v.multiply(5);
+        v.addValues(800, 900);
+        System.out.println(v); // < 810 , 915 >
+        
+        Vector w = new Vector (1,1);
+        System.out.println(w.getLength()); //1.4142135623730951
+        System.out.println(w.getAngle()); // 45.0 (deg)
+        
+        v.setLength(1.414); 
+        v.setAngle(45);
+        System.out.println(v);
+    }
+}
---
+++ Bagel/src/bagel/Vector.java
@@ -0,0 +1,77 @@
+package bagel;
+
+public class Vector 
+{
+    public double x;
+    public double y;
+    
+    public Vector()
+    {
+        setValues(0,0);
+    }
+    
+    public Vector(double x, double y)
+    {
+        setValues(x,y);
+    }
+    
+    private void setValues(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void addVector(Vector other)
+    {
+        this.x += other.x;
+        this.y += other.y;
+    }
+    
+    public void addValues(double dx, double dy)
+    {
+        this.x += dx;
+        this.y += dy;
+    }
+    
+    public void multiply(double scalar)
+    {
+        this.x *= scalar;
+        this.y *= scalar;
+    }
+    
+    public double getLength()
+    {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    
+    // returns angles in degrees
+    public double getAngle()
+    {
+         if (this.getLength() == 0)
+            return 0;
+        else
+            return Math.toDegrees( Math.atan2(this.y, this.x) );
+    }
+    
+    public void setLength(double length)
+    {
+        double angleDegrees = getAngle();
+        double angleRad = Math.toRadians(angleDegrees);
+        this.x = length * Math.cos(angleRad);
+        this.y = length * Math.sin(angleRad);
+    }
+    
+    public void setAngle(double angleDeg)
+    {
+        double length = getLength();
+        double angleRad = Math.toRadians(angleDeg);
+        this.x = length * Math.cos(angleRad);
+        this.y = length * Math.sin(angleRad);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "< " + this.x + " , " + this.y + " >";
+    }
+}
